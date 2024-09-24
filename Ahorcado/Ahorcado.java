0c962257a9c9fcc61222b52ac951737dd9dd376f
Ruben_BV <r.blanco.3d@gmail.com>
2024-09-24 12:25:39 +0200

    Iniciando / Lista de palabras / Seleccionando palabra aleatoria

--- Ahorcado/Ahorcado.java
+++ Ahorcado/Ahorcado.java
@@ -1,3 +1,18 @@
+import java.util.Random;
+
 public class Ahorcado {
     
+    public static void main(String[] args) {
+
+        // Creamos array con lista de palabras
+        String[] lista_palabras={"perro", "gato", "canario", "cobaya"};
+        int long_array = lista_palabras.length;
+
+        Random random = new Random();
+        int random_posicion = random.nextInt(long_array);
+
+        String palabra_elegida = lista_palabras[random_posicion];
+
+        System.out.println(palabra_elegida);
+    }
 }
