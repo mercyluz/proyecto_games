6cfad3296904a4724318b0ea176c1e6d57394715
mercyluz <mercyluz@hotmail.com>
2024-09-24 05:17:42 -0700

    cambios ahorcado

--- Ahorcado/Ahorcado.java
+++ Ahorcado/Ahorcado.java
@@ -1,5 +1,5 @@
 import java.util.Random;
-
+import java.util.Scanner;
 public class Ahorcado {
     
     public static void main(String[] args) {
@@ -7,12 +7,19 @@ public class Ahorcado {
         // Creamos array con lista de palabras
         String[] lista_palabras={"perro", "gato", "canario", "cobaya"};
         int long_array = lista_palabras.length;
-
+        int intentosMaximos = 6;
+        int countIntentos =0;
         Random random = new Random();
         int random_posicion = random.nextInt(long_array);
 
         String palabra_elegida = lista_palabras[random_posicion];
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("dime una letra" );
+            String letra = scanner.nextLine();
 
-        System.out.println(palabra_elegida);
-    }
+       for (int i= 0; i < palabra_elegida.length(); i++);
+            if (letra = String palabra_elegida.chartAt(i)){
+
+            }
+
 }
