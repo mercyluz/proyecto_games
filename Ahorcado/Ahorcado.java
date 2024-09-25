9f3bfe9c679840dea9df2bf7ed31bc49132861dd
Ruben_BV <r.blanco.3d@gmail.com>
2024-09-25 08:55:35 +0200

    Selección de palabra al azar

--- Ahorcado/Ahorcado.java
+++ Ahorcado/Ahorcado.java
@@ -1,18 +1,42 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Ahorcado {
     
     public static void main(String[] args) {
 
-        // Creamos array con lista de palabras
-        String[] lista_palabras={"perro", "gato", "canario", "cobaya"};
-        int long_array = lista_palabras.length;
+        
+        String[] lista_palabras={"perro", "gato", "canario", "cobaya"}; // Creamos array con lista de palabras.
+        String palabra_elegida = "";    // Variable que guarda la palabra aleatoriamente elegida por el programa para se adivinada por el jugador.
+        int random_posicion;    // Es la posición numérica de la lista de palabras que define a la palabra elegida.
+        int intentosMaximos = 6;    // Variable que define el número de intentos máximos que tendrá el jugador para adivinar la palabra.
+        int countIntentos = 0;  // Se inicia la variable que contabiliza el número de intentos utilizados por el jugador.
+        
 
+        // El programa selecciona una palabra de la lista al azar.
+        // Esta palabra corresponde con una posición del array "lista_palabras"
         Random random = new Random();
-        int random_posicion = random.nextInt(long_array);
+        random_posicion = random.nextInt(lista_palabras.length);
 
-        String palabra_elegida = lista_palabras[random_posicion];
+        palabra_elegida = lista_palabras[random_posicion];
 
-        System.out.println(palabra_elegida);
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Dime una letra");
+        String letra = scanner.nextLine();
+
+
+        // letra = Character.toString();
+        // char letra = Character.toLowerCase(scanner.next().carAt(0));
+
+        for (int i = 0; i < palabra_elegida.length(); i++){
+
+            if(letra == Character.toString(palabra_elegida.charAt(i))){
+
+            }
+        }
+            
+
+        scanner.close();
     }
 }
